e742261d17ae47af8e6d4d4f1e422a5157d4844f
Valerio Billi <valerio.billi18@gmail.com>
2023-04-26 16:42:31 +0200

    add numbers

--- js/main.js
+++ js/main.js
@@ -4,17 +4,34 @@
 
 // aggiungo la funzione che cambia colore al click
 
-const griglia = document.getElementById("grid");
+const grid = document.getElementById("grid");
 
 const start = document.getElementById("start");
 
 start.addEventListener("click", addSquares);
 
+let square = document.getElementsByClassName("square100");
+
+// square.addEventListener("click", function() {
+//     this.classList.toggle("selected")
+// });
+
+
+// newSquare.addEventListener("click", selectSquare);
+
+
+// funzione per generare i quadrati
 function addSquares() {
     for (let i = 0; i < 100; i++) {
         const newSquare = document.createElement("div");
         newSquare.classList.add("square100");
-        griglia.appendChild(newSquare);
-        
+        grid.appendChild(newSquare);
+        newSquare.innerHTML = i+1; 
     }
 }
+
+// function selectSquare() {
+//     document.getElementsByClassName("square100").classList.toggle("selected");
+//     console.log("Hai selezionato la casella numero:")
+// }
+
