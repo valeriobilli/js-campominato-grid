377c63f0152d15f2cf7d70dd0dd2a36491c9c01f
Valerio Billi <valerio.billi18@gmail.com>
2023-04-26 16:08:33 +0200

    funzione genera quadrati

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-console.log("prova");
+// creo la funzione
+
+// associo al bottono l'effetto della funzione
+
+// aggiungo la funzione che cambia colore al click
+
+const griglia = document.getElementById("grid");
+
+const start = document.getElementById("start");
+
+start.addEventListener("click", addSquares);
+
+function addSquares() {
+    for (let i = 0; i < 100; i++) {
+        const newSquare = document.createElement("div");
+        newSquare.classList.add("square100");
+        griglia.appendChild(newSquare);
+        
+    }
+}
