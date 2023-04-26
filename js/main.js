7dc7eda3b82b1675dc14f05e29d03fb97863da8e
Valerio Billi <valerio.billi18@gmail.com>
2023-04-26 17:12:51 +0200

    modifiche varie

--- js/main.js
+++ js/main.js
@@ -10,28 +10,20 @@ const start = document.getElementById("start");
 
 start.addEventListener("click", addSquares);
 
-let square = document.getElementsByClassName("square100");
-
-// square.addEventListener("click", function() {
-//     this.classList.toggle("selected")
-// });
-
-
-// newSquare.addEventListener("click", selectSquare);
-
 
 // funzione per generare i quadrati
 function addSquares() {
     for (let i = 0; i < 100; i++) {
-        const newSquare = document.createElement("div");
+        var newSquare = document.createElement("div");
         newSquare.classList.add("square100");
         grid.appendChild(newSquare);
         newSquare.innerHTML = i+1; 
     }
 }
 
-// function selectSquare() {
-//     document.getElementsByClassName("square100").classList.toggle("selected");
-//     console.log("Hai selezionato la casella numero:")
-// }
+// funzione per selezionare i quadrati
+
+
+
+
 
